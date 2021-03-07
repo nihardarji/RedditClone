010cbe7beab532fbbc25607cc4de8f1cc4ab60d5
nihardarji <ndarji@hawk.iit.edu>
2021-03-06 19:43:12 -0600

    added basic resolvers for graphql

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,17 +1,24 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType } from "type-graphql";
 
+@ObjectType()  // For Graphql
 @Entity()
 export class Post {
 
+    // Field can be used to expose the field to graphql
+    @Field()
     @PrimaryKey()
     id!: number;
 
+    @Field(() => String)
     @Property({ type: 'date' })
     createdAt = new Date();
 
+    @Field(() => String)
     @Property({ type: 'date', onUpdate: () => new Date() })
     updatedAt = new Date();
 
+    @Field()
     @Property({ type: 'text' })
     title!: string;
 
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,32 @@
+import 'reflect-metadata'
+import express from 'express'
 import { MikroORM } from '@mikro-orm/core'
 import { __prod__ } from './constants'
-import { Post } from './entities/Post'
 import microConfig from './mikro-orm.config'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { PostResolver } from './resolvers/postResolver'
 
-const main = async () => {
+(async () => {
     const orm = await MikroORM.init(microConfig)
     await orm.getMigrator().up()
 
-    const posts = await orm.em.find(Post, {})
-    console.log(posts)
-}
+    const app = express()
 
-main()
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [ PostResolver ],
+            validate: false
+        }),
+        // Context: special object that is accessible in all resolvers
+        context: () => ({ em: orm.em })
+    })
+
+    apolloServer.applyMiddleware({ app }) 
+
+    const PORT = process.env.PORT || 5000
+    app.listen(PORT, () => {
+        console.log(`Server started running on port ${PORT}`)
+    })
+})()
     .catch(err => console.log(err))
