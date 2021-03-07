a0a0413ce84088f83bba44649b6d248e6253ccca
nihardarji <ndarji@hawk.iit.edu>
2021-03-06 20:13:16 -0600

    added update & delete mutation

--- src/resolvers/postResolver.ts
+++ src/resolvers/postResolver.ts
@@ -1,5 +1,5 @@
 import { Post } from '../entities/Post'
-import { Arg, Ctx, Int, Query, Resolver } from 'type-graphql'
+import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql'
 import { MyContext } from '../types'
 
 @Resolver()
@@ -11,9 +11,45 @@ export class PostResolver {
 
     @Query(() => Post, { nullable: true })
     post(
-        @Arg("id", () => Int) id: number, 
+        @Arg("id") id: number, 
         @Ctx() { em }: MyContext
     ): Promise<Post | null> {
         return em.findOne(Post, { id })
+    }
+
+    @Mutation(() => Post)
+    async createPost(
+        @Arg("title") title: string, 
+        @Ctx() { em }: MyContext
+    ): Promise<Post> {
+        const post = em.create(Post, { title })
+        await em.persistAndFlush(post)
+        return post
+    }
+
+    @Mutation(() => Post, { nullable: true })
+    async updatePost(
+        @Arg("id") id: number,
+        @Arg("title") title: string,
+        @Ctx() { em }: MyContext
+    ): Promise<Post | null> {
+        const post = await em.findOne(Post, { id })
+        if(!post) {
+            return null
+        }
+        if(typeof title !== 'undefined'){
+            post.title = title
+            await em.persistAndFlush(post)
+        }
+        return post
+    }
+
+    @Mutation(() => Boolean)
+    async deletePost(
+        @Arg("id") id: number,
+        @Ctx() { em }: MyContext
+    ): Promise<boolean> {
+        await em.nativeDelete(Post, { id })
+        return true
     }
 }
